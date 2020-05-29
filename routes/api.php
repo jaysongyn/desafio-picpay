3865d647efdfd358770ed333b73a02d8154e592e
jayson <jayson.inf@gmail.com>
2020-05-29 11:13:46 -0300

    improve route using group

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,8 @@ $router->group(['prefix' => 'users'], function () use ($router) {
     $router->post('/consumers', 'ConsumerController@store');
     $router->post('/sellers', 'SellerController@store');
 });
-
-$router->get('/transactions/{transaction_id}', 'TransactionController@show');
-$router->post('/transactions', 'TransactionController@store');
-$router->post('/transactions/authorize', 'TransactionController@authorizeTransaction');
+$router->group(['prefix' => 'transactions'], function () use ($router) {
+    $router->get('/{transaction_id}', 'TransactionController@show');
+    $router->post('', 'TransactionController@store');
+    $router->post('/authorize', 'TransactionController@authorizeTransaction');
+});
